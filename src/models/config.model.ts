4fcdd63cdbece918c50478028a4005b1f1635f43
Josh Caponigro <joshcaponigro@protonmail.com>
2025-10-01 01:19:57 -0500

    Add config models

---
+++ src/models/config.model.ts
@@ -0,0 +1,23 @@
+export interface APIConfiguration {
+    host: string,
+    port: number,
+    api_key: string,
+    is_ssl: boolean,
+}
+
+export interface UIConfiguration {
+    ui_refresh_rate: number,
+    ui_theme: string
+}
+
+export interface MonitoringConfiguration {
+    poll_interval: number,
+    retry_attempts: number,
+    retry_delay: number,
+}
+
+export interface Configuration {
+    api_configuration: APIConfiguration
+    monitoring_configuration: MonitoringConfiguration
+    ui_configuration: UIConfiguration
+}
